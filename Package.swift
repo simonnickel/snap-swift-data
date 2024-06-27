053785b587366ba7bcb007dec9d15083692d2f78
Simon Nickel <simonnickel@googlemail.com>
2024-06-27 15:17:26 +0200

    Raise swift and platform versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "snap-swift-data",
 	platforms: [
-		.iOS(.v17), .macOS(.v14)
+		.iOS(.v18), .macOS(.v15)
 	],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -19,14 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SnapSwiftData",
-			swiftSettings: [
-				.enableExperimentalFeature("StrictConcurrency")
-			]
+            name: "SnapSwiftData"
 		),
         .testTarget(
             name: "SnapSwiftDataTests",
             dependencies: ["SnapSwiftData"]
 		),
-    ]
+    ],
+	swiftLanguageVersions: [.version("6")]
 )
