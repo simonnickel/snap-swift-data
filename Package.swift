77ea60179854a31d5628385a50c2cf78beaff4b4
Simon Nickel <simonnickel@googlemail.com>
2026-04-25 21:52:18 +0200

    Update to Swift 6.3

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 6.3
 
 import PackageDescription
 
@@ -9,18 +8,14 @@ let package = Package(
 		.iOS(.v18), .macOS(.v15)
 	],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SnapSwiftData",
             targets: ["SnapSwiftData"]
 		),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SnapSwiftData"
 		),
     ],
-    swiftLanguageModes: [.version("6")]
 )
